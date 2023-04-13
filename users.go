caa15e55dc4831ca8a3a75a2da8bc01110ef623e
Mohamed Faris <mohd.faris07@gmail.com>
2023-04-13 18:18:23 +0300

    cleanup

--- users.go
+++ users.go
@@ -6,21 +6,21 @@ import (
 )
 
 type User struct {
-  Username string `json:"username"`
-  Email string `json:"email"`
-  FirstName string `json:"first_name"`
-  LastName string `json:"last_name"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 var users = []User{
-  {Username: "user1", Email: "test@test.c", FirstName: "John", LastName: "Doe"},
-  {Username: "user2", Email: "test2@test.c", FirstName: "Jane", LastName: "Doe"},
+	{Username: "user1", Email: "test@test.c", FirstName: "John", LastName: "Doe"},
+	{Username: "user2", Email: "test2@test.c", FirstName: "Jane", LastName: "Doe"},
 }
 
 func main() {
 	r := gin.Default()
-  r.GET("/users", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"users": users})
-  })
-    r.Run(":8080")
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"users": users})
+	})
+	r.Run(":8080")
 }
